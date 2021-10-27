01f669f1825385ef246bf67c133d64e758cf0cea
JaiirC <jairskater02@hotmail.com>
2021-10-27 11:58:52 -0500

    creacion de tablas

---
+++ citas.sql
@@ -0,0 +1,50 @@
+/* creacion de base datos*/
+ create database prueba2;
+
+/*creacion de las tablas*/
+ create table paciente(ci_paciente varchar(10) not null,
+					   nombre_paciente varchar(10) not null,
+					   apellido_paciente varchar(10) not null,
+					   direccion_paciente varchar(10) not null,
+					   tlf_paciente varchar(10) not null,
+					   primary key (ci_paciente)
+					  );
+ alter table paciente add provincia varchar (20) not null;
+ alter table paciente rename column provincia to ciudad;
+
+ create table medico(ci_medico varchar(10) not null,
+					   nombre_medico varchar(10) not null,
+					   apellido_medico varchar(10) not null,
+					   especialidad varchar(10) not null,
+					   tlf_medico varchar(10) not null,
+					   primary key (ci_medico)
+					  );
+					  
+  create table especialidad(id_especialidad varchar(10) not null,
+							nombre varchar(10) not null,
+							primary key (id_especialidad)
+						   );
+																					
+ create table citas(codigo integer not null,
+					fecha date not null,
+					hora time not null,
+					ci_paciente varchar(10),
+					id_em integer not null,
+					primary key (codigo));
+					
+ alter table citas add foreign key (ci_paciente) references paciente(ci_paciente);
+ alter table citas add foreign key (id_em) references especialidadMedico(id_em);
+ 
+					
+ create table especialidadMedico(id_em integer not null,
+								 horarioini time not null,
+								 horariofin time not null,
+								 ci_medico varchar(10) not null,
+								 id_especialidad varchar(10) not null,
+								 primary key(id_em));
+ alter table especialidadMedico add foreign key (ci_medico) references medico(ci_medico);	
+ alter table especialidadMedico add foreign key (id_especialidad) references especialidad(id_especialidad);	
+ 
+						
+
+						   
